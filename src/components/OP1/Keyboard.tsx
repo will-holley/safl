56ee82b11a3767a3bb40d644acaafce2c18fb280
Will Holley <will@willholley.com>
2022-10-22 18:17:06 -0400

    refactor: reorder Key elements to better mimic their true layout

--- src/components/OP1/Keyboard.tsx
+++ src/components/OP1/Keyboard.tsx
@@ -16,34 +16,25 @@ const Layout = styled.div<{}>`
 const Keyboard: React.FC<{}> = ({}) => {
   return (
     <Layout>
-      <KeyGroup column={1} row={1}>
-        <BlackKey id="F#1" />
-        <BlackKey id="G#1" width={BlackKeyWidth.Short} />
-        <BlackKey id="A#1" />
-      </KeyGroup>
-      <KeyGroup column={2} row={1}>
-        <BlackKey id="C#1" />
-        <BlackKey id="D#1" />
-      </KeyGroup>
-      <KeyGroup column={3} row={1}>
-        <BlackKey id="F#2" />
-        <BlackKey id="G#2" width={BlackKeyWidth.Short} />
-        <BlackKey id="A#2" />
-      </KeyGroup>
-      <KeyGroup column={4} row={1}>
-        <BlackKey id="C#2" />
-        <BlackKey id="D#2" />
-      </KeyGroup>
       <KeyGroup column={1} row={2}>
         <WhiteKey id="F1" />
         <WhiteKey id="G1" />
         <WhiteKey id="A1" />
         <WhiteKey id="B1" />
       </KeyGroup>
+      <KeyGroup column={1} row={1}>
+        <BlackKey id="F#1" />
+        <BlackKey id="G#1" width={BlackKeyWidth.Short} />
+        <BlackKey id="A#1" />
+      </KeyGroup>
       <KeyGroup column={2} row={2}>
         <WhiteKey id="C1" />
         <WhiteKey id="D1" />
         <WhiteKey id="E1" />
+      </KeyGroup>
+      <KeyGroup column={2} row={1}>
+        <BlackKey id="C#1" />
+        <BlackKey id="D#1" />
       </KeyGroup>
       <KeyGroup column={3} row={2}>
         <WhiteKey id="F2" />
@@ -51,10 +42,19 @@ const Keyboard: React.FC<{}> = ({}) => {
         <WhiteKey id="A2" />
         <WhiteKey id="B2" />
       </KeyGroup>
+      <KeyGroup column={3} row={1}>
+        <BlackKey id="F#2" />
+        <BlackKey id="G#2" width={BlackKeyWidth.Short} />
+        <BlackKey id="A#2" />
+      </KeyGroup>
       <KeyGroup column={4} row={2}>
         <WhiteKey id="C2" />
         <WhiteKey id="D2" />
         <WhiteKey id="E2" />
+      </KeyGroup>
+      <KeyGroup column={4} row={1}>
+        <BlackKey id="C#2" />
+        <BlackKey id="D#2" />
       </KeyGroup>
     </Layout>
   );
